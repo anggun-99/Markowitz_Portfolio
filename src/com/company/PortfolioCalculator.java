bab06308f94e05a10e1ecf75762cd45b8c2d569d
Jauhar Bariq Ghifari <jauhar.bariq.ghifari@mnd.thm.de>
2021-05-10 13:55:14 +0000

    calculatedrisk

--- src/com/company/PortfolioCalculator.java
+++ src/com/company/PortfolioCalculator.java
@@ -19,8 +19,14 @@ public class PortfolioCalculator {
         return Math.pow((1+sum/(s.getStockPriceSize()-1)),250) - 1;
     }
 
-    public double calculateRiskForStocks() {
-        return 0;
+    public double calculateRiskForStocks(String symbol) {
+        Stock s = controller.getDataForSymbol(symbol);
+
+        double sum = 0;
+        for(int i =1; i< s.getStockPriceSize(); i++){
+            sum += ((s.getStockPriceAt(i).getPrice() - s.getStockPriceAt(i-1).getPrice())/s.getStockPriceAt(i-1).getPrice()-calculateRenditeForStock(symbol)/250)^2
+        }
+        return Math.sqrt(sum/(s.getStockPriceSize()-1));
     }
 
     public double calculateCorrelationOfStocks() {
